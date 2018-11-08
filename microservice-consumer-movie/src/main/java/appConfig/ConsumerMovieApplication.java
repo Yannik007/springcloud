33beac6ee8cd89fedc9c697251ea411186ccc6be
Tboss <472680811@qq.com>
2018-11-08 11:16:30 +0800

    解决无法install

---
+++ microservice-consumer-movie/src/main/java/appConfig/ConsumerMovieApplication.java
@@ -0,0 +1,20 @@
+package appConfig;
+/*
+* @Description :
+* @Date : 2018/6/26  14:55  
+* */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author tyw
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ConsumerMovieApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerMovieApplication.class, args);
+    }
+}
